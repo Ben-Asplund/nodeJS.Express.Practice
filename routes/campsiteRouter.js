e92e67cf19ea4943e42cd52582af04424b6b1c5e
Ben Asplund <basplund87@gmail.com>
2022-11-05 11:21:48 -0500

    campsite id added to campsite router

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -29,4 +29,31 @@ campsiteRouter.route('/')
 
 //app and routing ie app.all('/campsites) deleted because now they are all chained to the campsiteRouter as one
 
+campsiteRouter.route('/:campsiteId')
+
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => { //allows us to store whatever the client sends as part of the path after the slash as a route parameter named campsiteId
+    res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
+})
+
+.post((req, res) => {
+    res.statusCode = 403;
+    res.end(`POST operation not supported on /campsites/${req.params.campsiteId} `);
+})
+
+.put((req, res) => {
+    res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
+    res.end(`Will update the campsite: ${req.body.name} 
+        with description: ${req.body.description}`);
+})
+
+.delete((req,res) => {
+    res.end(`Deleting campsite: ${req.params.campsiteId}`);
+});
+
+
 module.exports = campsiteRouter; //now it can be used elsewhere 
